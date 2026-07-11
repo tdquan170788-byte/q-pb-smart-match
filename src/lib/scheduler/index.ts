a7bf1b566fb3a021bcdc8ed150e3b9a4c3f3f591
tdquan170788-byte <tdquan170788@gmail.com>
2026-07-11 14:44:52 +0700

    Update index.ts
    Sprint 0711 0244

--- src/lib/scheduler/index.ts
+++ src/lib/scheduler/index.ts
@@ -1,6 +1,7 @@
 export * from "./engine";
 export * from "./helpers";
 export * from "./generator";
+export * from "./candidates";
 export * from "./validator";
 export * from "./cost";
 export * from "./optimizer";
