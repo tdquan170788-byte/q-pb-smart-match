85d695ee66f8f322c19ab4bc3b9f1f41c9e52b29
tdquan170788-byte <tdquan170788@gmail.com>
2026-07-22 00:21:21 +0700

    Update types.ts

--- src/lib/statistics/session-summary/types.ts
+++ src/lib/statistics/session-summary/types.ts
@@ -53,6 +53,14 @@ export interface PlayerSessionSummary {
 
   winRate: number;
 
+  currentWinStreak: number;
+
+  currentLoseStreak: number;
+
+  longestWinStreak: number;
+
+  longestLoseStreak: number;
+
   ratingBefore?: number;
 
   ratingAfter?: number;
